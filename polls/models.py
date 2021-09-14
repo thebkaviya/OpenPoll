ab5d888f64637349cb06ffe715a375ce4e64a9e3
Tarith <tarithj@gmail.com>
2021-09-14 14:21:24 +0530

    commit poll app

---
+++ polls/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Question(models.Model):
+    id = models.CharField("id", max_length=10, primary_key=True)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
