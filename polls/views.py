3559226256a84516a363554e3ea388aa0b0fa7c6
Tarith <tarithj@gmail.com>
2021-09-15 21:17:11 +0530

    changed IndexView from ListView to TemplateView

--- polls/views.py
+++ polls/views.py
@@ -6,14 +6,9 @@ from django.views import generic
 from .models import Choice, Question
 
 
-class IndexView(generic.ListView):
+class IndexView(generic.TemplateView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
 
 class DetailView(generic.DetailView):
     model = Question
