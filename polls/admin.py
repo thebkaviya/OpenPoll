becc36a13e52fa3c734ee73bbc2cd2b6ee66f2c6
Tarith <tarithj@gmail.com>
2021-09-21 18:24:09 +0530

    changed admin site title to "OpenPoll"

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 admin.site.site_header = 'OpenPoll Admin'
+admin.site.site_title = 'OpenPoll'
+
 admin.site.register(Question)
 admin.site.register(Choice)
